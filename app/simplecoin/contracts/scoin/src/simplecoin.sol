3d22d7b1e61e6beb37bfdf04acaf4541ca31bbd7
William Kennedy <bill@ardanlabs.com>
2022-06-07 13:53:33 -0400

    saving partial changes

--- app/simplecoin/contracts/scoin/src/simplecoin.sol
+++ app/simplecoin/contracts/scoin/src/simplecoin.sol
@@ -3,19 +3,33 @@ pragma solidity ^0.8.0;
 
 contract SimpleCoin {
     mapping (address => uint256) public coinBalance;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    // =========================================================================
 
     event Log(string value);
     event Transfer(address indexed from, address indexed to, uint256 value);
 
+    // =========================================================================
+
     constructor(uint256 initialSupply) {
         coinBalance[msg.sender] = initialSupply;
     }
+
+    // =========================================================================
 
     function transfer(address to, uint256 amount) external {
 
         // Check that the sender has enough coins.
         if (coinBalance[msg.sender] < amount) {
             string memory resp = string(abi.encodePacked("insufficent funds  bal:", uint2str(coinBalance[msg.sender]), "  amount: ", uint2str(amount)));
+            revert(resp);
+        }
+
+        // Check the amount is not zero or the amount value caused the unsigned
+        // int to restart back to zero.
+        if (coinBalance[to]+amount <= coinBalance[to]) {
+            string memory resp = string(abi.encodePacked("invalid amount: ", uint2str(amount)));
             revert(resp);
         }
 
@@ -28,6 +42,15 @@ contract SimpleCoin {
 
         emit Transfer(msg.sender, to, amount);
     }
+
+    // authorize allows the sender of this call to be authorized to spend the
+    // specified accounts money up to the allowance amount.
+    function authorize(address authorizedAccount, uint256 allowanceAmount) public returns (bool) {
+        allowance[msg.sender][authorizedAccount] = allowanceAmount;
+        return true;
+    }
+
+    // =========================================================================
 
     function uint2str(uint i) internal pure returns (string memory) {
         if (i == 0) {
