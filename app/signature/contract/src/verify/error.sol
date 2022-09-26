4c5b72622f7c8041acea8d52e32a54488bf89a4a
William Kennedy <bill@ardanlabs.com>
2022-09-26 15:07:39 -0400

    added error handling

---
+++ app/signature/contract/src/verify/error.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+library Error {
+    struct Err {
+        bool   isError;
+        string msg;
+    }
+
+    // None returns an error value indicating no error.
+    function None() internal pure returns (Err memory) {
+        return Err({isError: false, msg: ""});
+    }
+
+    // New constructs an error value with a set of varadic parameters.
+    function New(string memory a, string memory b, string memory c, string memory d) internal pure returns (Err memory) {
+        return Err({isError: true, msg: string.concat(a, b, c, d)});
+    }
+
+    // New constructs an error value with a set of varadic parameters.
+    function New(string memory a, string memory b, string memory c) internal pure returns (Err memory) {
+        return Err({isError: true, msg: string.concat(a, b, c)});
+    }
+
+    // New constructs an error value with a set of varadic parameters.
+    function New(string memory a, string memory b) internal pure returns (Err memory) {
+        return Err({isError: true, msg: string.concat(a, b)});
+    }
+
+    // New constructs an error value with a set of varadic parameters.
+    function New(string memory a) internal pure returns (Err memory) {
+        return Err({isError: true, msg: a});
+    }
+
+    // Itoa converts an unsigned integer to a string.
+    function Itoa(uint i) internal pure returns (string memory) {
+        if (i == 0) {
+            return "0";
+        }
+
+        uint j = i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(i - i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            i /= 10;
+        }
+
+        return string(bstr);
+    }
+
+    // Addrtoa converts an address to a string.
+    function Addrtoa(address x) internal pure returns (string memory) {
+        bytes memory s = new bytes(40);
+
+        for (uint i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2*i] = char(hi);
+            s[2*i+1] = char(lo);
+        }
+
+        return string(s);
+    }
+
+    // char is a support function for Addresstoa.
+    function char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) {
+            return bytes1(uint8(b) + 0x30);
+        }
+        return bytes1(uint8(b) + 0x57);
+    }
+}
--- app/signature/contract/src/verify/verify.sol
+++ app/signature/contract/src/verify/verify.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "./error.sol";
+
 // Verify is a contract which can verify that a hashed message matches the
 // provided signer.
 contract Verify {
@@ -8,22 +10,36 @@ contract Verify {
     // Address retrieves the signer's address from a signature that was produced
     // based on the provided data that was signed.
     function Address(bytes memory data, bytes calldata sig) public pure returns (address) {
-        return extractAddress(data, sig);
+        (address addr,Error.Err memory err) = extractAddress(data, sig);
+        if (err.isError) {
+            revert(err.msg);
+        }
+
+        return addr;
     }
 
     // MatchSender checks the address of the sender was the address that signed
     // the specified data with the provided signature.
     function MatchSender(bytes memory data, bytes calldata sig) public view returns (bool) {
-        address addr = extractAddress(data, sig);
+        (address addr,Error.Err memory err) = extractAddress(data, sig);
+        if (err.isError) {
+            revert(err.msg);
+        }
+
         if (addr == msg.sender) {
             return true;
         }
+
         return false;
     }
 
     // extractAddress expects the raw data that was signed and will apply the Ethereum
     // salt value manually. This hides the underlying implementation of the salt.
-    function extractAddress(bytes memory data, bytes calldata sig) private pure returns (address) {
+    function extractAddress(bytes memory data, bytes calldata sig) private pure returns (address, Error.Err memory) {
+        if (sig.length != 65) {
+            return (address(0), Error.New("invalid signature length"));
+        }
+
         bytes32 hashedData = keccak256(data);
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         bytes32 saltedData = keccak256(abi.encodePacked(prefix, hashedData));
@@ -32,6 +48,6 @@ contract Verify {
         bytes32 s = bytes32(sig[32:64]);
         uint8 v = uint8(sig[64]);
 
-        return ecrecover(saltedData, v, r, s);
+        return (ecrecover(saltedData, v, r, s), Error.None());
     }
 }
