18474b2176cb544b8e46f9f08235f4380422a59f
bhechinger <wonko@4amlunch.net>
2022-11-15 10:49:52 +0000

    SimpleCoin tests written

--- app/simplecoin/contract/src/simplecoin/simplecoin_test.go
+++ app/simplecoin/contract/src/simplecoin/simplecoin_test.go
@@ -1,10 +1,10 @@
-package bank
+package simplecoin
 
-//go:generate ethier gen bank.sol error.sol
+//go:generate ethier gen simplecoin.sol error.sol
 
 import (
 	"context"
-	"fmt"
+	"math/big"
 	"testing"
 
 	"github.com/divergencetech/ethier/ethtest"
@@ -12,7 +12,9 @@ import (
 )
 
 const (
-	deployer = iota
+	startingBalance = 10000
+	transferAmount  = 100
+	deployer        = iota
 	account1
 	account2
 	account3
@@ -20,52 +22,54 @@ const (
 	numAccounts
 )
 
-func TestBank(t *testing.T) {
+func TestSimpleCoin(t *testing.T) {
 	sim, err := ethtest.NewSimulatedBackend(numAccounts)
 	if err != nil {
 		t.Fatalf("Something went wrong settup up the simulated backend: %s", err)
 	}
 
-	_, _, contract, err := DeployBank(sim.Acc(deployer), sim /*,,, [constructor arguments]*/)
+	_, _, contract, err := DeploySimpleCoin(sim.Acc(deployer), sim, big.NewInt(startingBalance))
 	if err != nil {
 		t.Fatalf("DeployBank error %v", err)
 	}
 
-	var key [32]byte
-	var value [32]byte
-	copy(key[:], []byte("name"))
-	copy(value[:], []byte("brianna"))
-
-	t.Run("Test Bank", func(t *testing.T) {
+	t.Run("Test SimpleCoin", func(t *testing.T) {
 		callOpts := &bind.CallOpts{
 			Pending: true,
 			From:    sim.Addr(deployer),
 			Context: context.Background(),
 		}
 
-		balance, err := contract.Balance(callOpts)
+		balance, err := contract.CoinBalance(callOpts, sim.Addr(deployer))
 		if err != nil {
 			t.Fatalf("Error getting balance before deposit: %s", err)
 		}
 
-		fmt.Printf("balance before deposit: %v\n", balance)
+		if balance.Cmp(big.NewInt(startingBalance)) != 0 {
+			t.Fatalf("Deployer starting balance doesn't match: %v != %v", balance, startingBalance)
+		}
 
-		// proxyContract, err := NewBank(sim.Addr(deployer), sim)
-		// if err != nil {
-		// 	t.Fatalf("new proxy connection: %s", err)
-		// }
-
-		_, err = contract.Deposit(sim.Acc(deployer))
+		_, err = contract.Transfer(sim.Acc(deployer), sim.Addr(account1), big.NewInt(transferAmount))
 		if err != nil {
 			t.Fatalf("Error setting test data: %s", err)
 		}
 
-		balance, err = contract.Balance(callOpts)
+		balance, err = contract.CoinBalance(callOpts, sim.Addr(deployer))
 		if err != nil {
 			t.Fatalf("Error getting balance after deposit: %s", err)
 		}
 
-		fmt.Printf("balance after deposit: %v\n", balance)
+		if balance.Cmp(big.NewInt(startingBalance-transferAmount)) != 0 {
+			t.Fatalf("Deployer ending balance doesn't match: %v != %v", balance, startingBalance)
+		}
 
+		balance, err = contract.CoinBalance(callOpts, sim.Addr(account1))
+		if err != nil {
+			t.Fatalf("Error getting balance after deposit: %s", err)
+		}
+
+		if balance.Cmp(big.NewInt(transferAmount)) != 0 {
+			t.Fatalf("Account1 ending balance doesn't match: %v != %v", balance, transferAmount)
+		}
 	})
 }
