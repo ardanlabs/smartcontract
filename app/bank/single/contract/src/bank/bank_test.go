434f027614fe0610052fc93fd5b675e3566dcc2e
bhechinger <wonko@4amlunch.net>
2022-11-15 10:22:12 +0000

    started simple bank tests but deposit doesn't work

--- app/bank/single/contract/src/bank/bank_test.go
+++ app/bank/single/contract/src/bank/bank_test.go
@@ -1,27 +1,34 @@
-package store
+package bank
 
-//go:generate ethier gen store.sol
+//go:generate ethier gen bank.sol error.sol
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/divergencetech/ethier/ethtest"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 )
 
 const (
 	deployer = iota
+	account1
+	account2
+	account3
+	account4
 	numAccounts
 )
 
-func TestMyContract(t *testing.T) {
+func TestBank(t *testing.T) {
 	sim, err := ethtest.NewSimulatedBackend(numAccounts)
 	if err != nil {
 		t.Fatalf("Something went wrong settup up the simulated backend: %s", err)
 	}
 
-	_, _, contract, err := DeployStore(sim.Acc(deployer), sim /*,,, [constructor arguments]*/)
+	_, _, contract, err := DeployBank(sim.Acc(deployer), sim /*,,, [constructor arguments]*/)
 	if err != nil {
-		t.Fatalf("DeployStore error %v", err)
+		t.Fatalf("DeployBank error %v", err)
 	}
 
 	var key [32]byte
@@ -29,20 +36,36 @@ func TestMyContract(t *testing.T) {
 	copy(key[:], []byte("name"))
 	copy(value[:], []byte("brianna"))
 
-	t.Run("protect something sensitive", func(t *testing.T) {
-		_, err := contract.SetItem(sim.Acc(deployer), key, value)
+	t.Run("Test Bank", func(t *testing.T) {
+		callOpts := &bind.CallOpts{
+			Pending: true,
+			From:    sim.Addr(deployer),
+			Context: context.Background(),
+		}
+
+		balance, err := contract.Balance(callOpts)
+		if err != nil {
+			t.Fatalf("Error getting balance before deposit: %s", err)
+		}
+
+		fmt.Printf("balance before deposit: %v\n", balance)
+
+		// proxyContract, err := NewBank(sim.Addr(deployer), sim)
+		// if err != nil {
+		// 	t.Fatalf("new proxy connection: %s", err)
+		// }
+
+		_, err = contract.Deposit(sim.Acc(deployer))
 		if err != nil {
 			t.Fatalf("Error setting test data: %s", err)
 		}
 
-		items, err := contract.Items(nil, key)
+		balance, err = contract.Balance(callOpts)
 		if err != nil {
-			t.Fatalf("Error getting test data: %s", err)
+			t.Fatalf("Error getting balance after deposit: %s", err)
 		}
 
-		if items != value {
-			t.Fatalf("Expected %s, got %s", string(value[:]), string(items[:]))
-		}
+		fmt.Printf("balance after deposit: %v\n", balance)
 
 	})
 }
