e5362ced81b64a1251ba24534589bc0fd8db6744
bhechinger <wonko@4amlunch.net>
2022-11-16 11:54:01 +0000

    issues using CallOpts

--- app/bank/single/contract/go/bank/bank_test.go
+++ app/bank/single/contract/go/bank/bank_test.go
@@ -1,43 +1,75 @@
 package bank_test
 
 import (
+	"context"
 	"math/big"
 	"testing"
 
+	"github.com/ardanlabs/ethereum"
 	"github.com/ardanlabs/smartcontract/app/bank/single/contract/go/bank"
-	"github.com/divergencetech/ethier/ethtest"
 )
 
-const deployer = 0
-
 func TestBank(t *testing.T) {
-	sim, err := ethtest.NewSimulatedBackend(1)
+	ctx := context.Background()
+
+	sim, err := ethereum.CreateSimulation(1, true)
 	if err != nil {
 		t.Fatalf("unable to create simulated backend: %s", err)
 	}
+	defer sim.Close()
 
-	contractID, _, _, err := bank.DeployBank(sim.Acc(deployer), sim)
+	eth := ethereum.NewSimulation(sim, sim.PrivateKeys[0])
+
+	const (
+		gasLimit  = 1600000
+		valueGwei = 0.0
+	)
+
+	tranOpts, err := eth.NewTransactOpts(ctx, gasLimit, big.NewFloat(valueGwei))
+	if err != nil {
+		t.Fatalf("unable to create transaction opts for deploy: %s", err)
+	}
+
+	contractID, _, _, err := bank.DeployBank(tranOpts, eth.ContractBackend())
 	if err != nil {
 		t.Fatalf("unable to deploy bank: %s", err)
 	}
 
-	bank, err := bank.NewBank(contractID, sim)
+	testBank, err := bank.NewBank(contractID, sim)
 	if err != nil {
 		t.Fatalf("unable to create a bank: %s", err)
 	}
 
-	balanceBefore, err := bank.Balance(sim.CallFrom(deployer))
+	callOpts, err := eth.NewCallOpts(ctx)
+	if err != nil {
+		t.Fatalf("unable to create call opts for owner: %s", err)
+	}
+
+	owner, err := testBank.Owner(callOpts)
+	if err != nil {
+		t.Fatalf("unable to get account owner: %s", err)
+	}
+
+	if owner != eth.Address() {
+		t.Fatalf("Retrieved owner doesn't match expectations: %v != %v", owner, eth.Address())
+	}
+
+	balanceBefore, err := testBank.Balance(callOpts)
 	if err != nil {
 		t.Fatalf("should get the balance: %s", err)
 	}
 
-	to := sim.Acc(deployer)
+	to, err := eth.NewTransactOpts(ctx, gasLimit, big.NewFloat(valueGwei))
+	if err != nil {
+		t.Fatalf("unable to create transaction opts for deposit: %s", err)
+	}
+
 	to.Value = big.NewInt(10)
-	if _, err = bank.Deposit(to); err != nil {
+	if _, err = testBank.Deposit(to); err != nil {
 		t.Fatalf("should be able to deposit money: %s", err)
 	}
 
-	expBal, err := bank.Balance(sim.CallFrom(deployer))
+	expBal, err := testBank.Balance(callOpts)
 	if err != nil {
 		t.Fatalf("should get balance after deposit: %s", err)
 	}
