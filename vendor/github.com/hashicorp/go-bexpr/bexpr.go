2d6474a9e0d67ea47312fb9d8e4d13b31fa2a5b8
William Kennedy <bill@ardanlabs.com>
2024-08-15 18:52:36 +0100

    upgraded deps

--- vendor/github.com/hashicorp/go-bexpr/bexpr.go
+++ vendor/github.com/hashicorp/go-bexpr/bexpr.go
@@ -1,7 +1,10 @@
-// bexpr is an implementation of a generic boolean expression evaluator.
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+// Package bexpr is an implementation of a generic boolean expression evaluator.
 // The general goal is to be able to evaluate some expression against some
-// arbitrary data and get back a boolean of whether or not the data
-// was matched by the expression
+// arbitrary data and get back a boolean indicating if the data was matched by
+// the expression
 package bexpr
 
 //go:generate pigeon -o grammar/grammar.go -optimize-parser grammar/grammar.peg
@@ -12,7 +15,7 @@ import (
 	"github.com/mitchellh/pointerstructure"
 )
 
-// HookFn provides a way to translate one reflect.Value to another during
+// ValueTransformationHookFn provides a way to translate one reflect.Value to another during
 // evaluation by bexpr. This facilitates making Go structures appear in a way
 // that matches the expected JSON Pointers used for evaluation. This is
 // helpful, for example, when working with protocol buffers' well-known types.
@@ -23,8 +26,14 @@ type Evaluator struct {
 	ast                     grammar.Expression
 	tagName                 string
 	valueTransformationHook ValueTransformationHookFn
+	unknownVal              *interface{}
+	expression              string
 }
 
+// CreateEvaluator is used to create and configure a new Evaluator, the expression
+// will be used by the evaluator when evaluating against any supplied datum.
+// The following Option types are supported:
+// WithHookFn, WithMaxExpressions, WithTagName, WithUnknownValue.
 func CreateEvaluator(expression string, opts ...Option) (*Evaluator, error) {
 	parsedOpts := getOpts(opts...)
 	var parserOpts []grammar.Option
@@ -41,11 +50,29 @@ func CreateEvaluator(expression string, opts ...Option) (*Evaluator, error) {
 		ast:                     ast.(grammar.Expression),
 		tagName:                 parsedOpts.withTagName,
 		valueTransformationHook: parsedOpts.withHookFn,
+		unknownVal:              parsedOpts.withUnknown,
+		expression:              expression,
 	}
 
 	return eval, nil
 }
 
+// Evaluate attempts to match the configured expression against the supplied datum.
+// It returns a value indicating if a match was found and any error that occurred.
+// If an error is returned, the value indicating a match will be false.
 func (eval *Evaluator) Evaluate(datum interface{}) (bool, error) {
-	return evaluate(eval.ast, datum, WithTagName(eval.tagName), WithHookFn(eval.valueTransformationHook))
+	opts := []Option{
+		WithTagName(eval.tagName),
+		WithHookFn(eval.valueTransformationHook),
+	}
+	if eval.unknownVal != nil {
+		opts = append(opts, WithUnknownValue(*eval.unknownVal))
+	}
+
+	return evaluate(eval.ast, datum, opts...)
+}
+
+// Expression can be used to return the initial expression used to create the Evaluator.
+func (eval *Evaluator) Expression() string {
+	return eval.expression
 }
